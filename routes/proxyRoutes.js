2a94fda439ec8bff072dc35407422d9d17873397
maurinicolas <mlopez@escorial.com.ar>
2025-06-13 12:50:47 -0300

    translation logic extracted into function

--- routes/proxyRoutes.js
+++ routes/proxyRoutes.js
@@ -6,29 +6,7 @@ app.get('/test/:id', async (req, res) => {
   const url = `https://devil-ai.translate.goog/api-personality-test/${id}?_x_tr_sl=en&_x_tr_tl=es&_x_tr_hl=es&_x_tr_pto=wapp`;
 
   try {
-    const browser = await puppeteer.launch({
-      headless: true,
-      args: ['--no-sandbox', '--disable-setuid-sandbox']
-    });
-    const page = await browser.newPage();
-    await page.goto(url, { waitUntil: 'networkidle0' });
-
-    await page.evaluate(() => {
-      const style = document.createElement('style');
-      style.innerHTML = `
-        iframe#gt-nvframe,
-        iframe[src*="translate.google.com"] {
-          display: none !important;
-        }
-        body {
-          margin-top: 0 !important;
-        }
-      `;
-      document.head.appendChild(style);
-    });
-
-    const content = await page.content();
-    await browser.close();
+    const content = fetchTranslatedPage(url)
 
     res.setHeader('Content-Type', 'text/html');
     res.send(content);
@@ -43,6 +21,18 @@ app.get('/result/:id', async (req, res) => {
   const url = `https://devil-ai.translate.goog/r/${id}?_x_tr_sl=en&_x_tr_tl=es&_x_tr_hl=es&_x_tr_pto=wapp`;
 
   try {
+    
+    const content = fetchTranslatedPage(url)
+
+    res.setHeader('Content-Type', 'text/html');
+    res.send(content);
+  } catch (error) {
+    console.error('Error:', error);
+    res.status(500).send('Error al cargar la página traducida con Puppeteer');
+  }
+});
+
+async function fetchTranslatedPage(url) {
     const browser = await puppeteer.launch({
       headless: true,
       args: ['--no-sandbox', '--disable-setuid-sandbox']
@@ -67,10 +57,5 @@ app.get('/result/:id', async (req, res) => {
     const content = await page.content();
     await browser.close();
 
-    res.setHeader('Content-Type', 'text/html');
-    res.send(content);
-  } catch (error) {
-    console.error('Error:', error);
-    res.status(500).send('Error al cargar la página traducida con Puppeteer');
-  }
-});
+    return content
+}
